20b3f7d8a54136f5bda625aec719217f9e54b9be
Aaditya0417 <148562936+Aaditya0417@users.noreply.github.com>
2024-04-19 07:06:12 +0530

    Delete users directory

--- users/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $con = pg_connect("host=localhost dbname=GymStore user=postgres password=Aaditya@1399");
-    // Check connection
-    if (!$con){
-        echo "Failed to connect to PostgreSQL: " . pg_last_error();
-    }
-?>
