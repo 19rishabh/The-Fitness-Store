115361badde0ca656306aaada69f7af9a44c90c1
Aaditya0417 <148562936+Aaditya0417@users.noreply.github.com>
2024-04-23 00:51:57 +0530

    Update db.php

--- users/db.php
+++ users/db.php
@@ -1,6 +1,6 @@
 <?php
 
-    $con = pg_connect("host=localhost dbname=GymStore user=postgres password=Aaditya@1399");
+    $con = pg_connect("host=localhost dbname=GymStore user=postgres password=aaditya");
     // Check connection
     if (!$con){
         echo "Failed to connect to PostgreSQL: " . pg_last_error();
