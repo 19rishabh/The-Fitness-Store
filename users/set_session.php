3f3aea7b02ac45108cdd42db07d1ceada7353ece
19rishabh <rishabhagrawal0623@gmail.com>
2024-04-23 08:29:15 +0530

    Update set_session.php

--- users/set_session.php
+++ users/set_session.php
@@ -1,5 +1,5 @@
 <?php
-require('db.php');
+require('db.php'); 
 require_once 'google-client/vendor/autoload.php';
 session_start();
 
@@ -12,13 +12,13 @@ if ($payload) {
     $username = $payload['name'];
     
     // Check if the user exists in the database
-    $query = "SELECT * FROM `users` WHERE email='$email'";
-    $result = mysqli_query($con, $query);
-    $rows = mysqli_num_rows($result);
+    $query = "SELECT * FROM users WHERE email=$1";
+    $result = pg_query_params($con, $query, array($email));
+    $rows = pg_num_rows($result);
     if ($rows == 0) {
         // Insert the user into the database
-        $query = "INSERT INTO `users` (username, email) VALUES ('$username', '$email')";
-        mysqli_query($con, $query);
+        $query = "INSERT INTO users (name, username, email, create_datetime) VALUES ($1, $2, $3, CURRENT_TIMESTAMP)";
+        pg_query_params($con, $query, array($username, $username, $email));
     }
     
     // Set the user session
