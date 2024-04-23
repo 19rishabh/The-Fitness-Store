8c1820fcad13ffc6cd6fe59f368a5f00ef184aa1
19rishabh <rishabhagrawal0623@gmail.com>
2024-04-23 08:15:16 +0530

    Update login.php

--- users/login.php
+++ users/login.php
@@ -78,7 +78,7 @@ if (isset($_POST['username'])) {
 }
 ?>
 
-<script src="js/script.js"></script>
+<script src="../js/script.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
 <script>
