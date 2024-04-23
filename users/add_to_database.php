8f92ab4a9db554bc240d182c3bb6757ba3dc5380
19rishabh <rishabhagrawal0623@gmail.com>
2024-04-23 07:58:16 +0530

    Update add_to_database.php

--- users/add_to_database.php
+++ users/add_to_database.php
@@ -7,7 +7,8 @@
 
     $user = $_SESSION['username'];
     $sql1 = "SELECT * FROM users WHERE username = $1";
-    $result = pg_query_params($db, $sql1, array($user));
+    $result = pg_query_params($con, $sql1, array($user));
+
     if ($result) {
         $row = pg_fetch_assoc($result);
         $id = $row['id'];
@@ -20,19 +21,15 @@
             $subtotalAmount = $order['subtotal_amount'];
             $invoiceNumber = $order['invoice_number'];
 
-            // Prepare the SQL statement with placeholders
             $sql = "INSERT INTO orders (price, title, quantity, subtotal_amount, date, invoice_number, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7)";
             
-            // Execute the prepared statement with parameters
-            $result = pg_query_params($db, $sql, array($price, $title, $quantity, $subtotalAmount, $date, $invoiceNumber, $id));
-            if ($result === false) {
-                echo "Error: " . pg_last_error($db);
+            $result = pg_query_params($con, $sql, array($price, $title, $quantity, $subtotalAmount, $date, $invoiceNumber, $id));
             }
         }
     } else {
-        echo "Error: " . pg_last_error($db);
+        echo "Error: " . pg_last_error($con);
     }
 
     // Close database connection
-    pg_close($db);
+    pg_close($con);
 ?> 
