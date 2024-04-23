b0eab44939205ae04360a82b9eb438cfad3df2f9
19rishabh <rishabhagrawal0623@gmail.com>
2024-04-23 07:26:40 +0530

    Update add_to_database.php

--- users/add_to_database.php
+++ users/add_to_database.php
@@ -1,5 +1,5 @@
 <?php
-    include 'db.php';
+    include 'db.php'; 
 
     // Get order details from POST request
     $orderDetails = json_decode(file_get_contents('php://input'), true);
@@ -7,26 +7,25 @@
     session_start();
 
     $user = $_SESSION['username'];
-    $sql1 = "SELECT * FROM users WHERE username = '$user'";
-    $result = $con->query($sql1);
-    while($row = $result->fetch_assoc()) {
-        $id = $row['id'];
-    }
+    $sql1 = "SELECT id FROM users WHERE username = '$user'";
+    $result = pg_query($con, $sql1);
+    $row = pg_fetch_assoc($result);
+    $id = $row['id'];
 
     // Insert order details into database
     $date = date('Y-m-d');
     foreach ($orderDetails as $order) {
-        $title = $order['title'];
+        $title = pg_escape_string($order['title']); // Escape special characters
         $price = $order['price'];
         $quantity = $order['quantity'];
         $subtotalAmount = $order['subtotal_amount'];
         $invoiceNumber = $order['invoice_number'];
         $sql = "INSERT INTO orders (price, title, quantity, subtotal_amount, date, invoice_number, user_id) VALUES ('$price', '$title', '$quantity', '$subtotalAmount', '$date', '$invoiceNumber', '$id')";
-        if ($con->query($sql) !== TRUE) {
-            echo "Error: " . $sql . "<br>" . $con->error;
+        if (!pg_query($con, $sql)) {
+            echo "Error: " . $sql . "<br>" . pg_last_error($con);
         }
     }
 
     // Close database connection
-    $con->close();
-?> 
+    pg_close($con);
+?>
